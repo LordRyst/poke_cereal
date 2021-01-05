1bd6274b7be5bd5f1f2bf2f178f885bf498cec4a
lordryst <lordryst@gmail.com>
2021-01-04 20:43:38 -0700

    Checked moves known and used.

--- src/poke_cereal.cpp
+++ src/poke_cereal.cpp
@@ -1,5 +1,5 @@
 #include <string>
-#include <vector>
+#include <set>
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,7 +9,9 @@
 #include "pokemon.h"
 #include "moves.h"
 
-nlohmann::json data;
+using namespace nlohmann;
+
+json data;
 
 int main(int argc, char* argv[])
 {
@@ -50,6 +52,61 @@ int main(int argc, char* argv[])
     read_moves(moves_in);
 
     read_species(pokemon_in);
+
+    // std::set<std::string> all_moves;
+    // for (auto& move : data["Moves"].items())
+    // {
+    //     all_moves.insert(move.key());
+    // }
+    // std::set<std::string> moves_used;
+    // for(auto& specie :  data.at("Species").items())
+    // {
+    //     for (auto& form : specie.value().at("Forms").items())
+    //     {
+    //         for(json::iterator move = form.value()["Moves"].begin(); move != form.value()["Moves"].end(); move++)
+    //         {
+    //             //printf("%s\n", move->dump().c_str());
+    //             std::string str = move.value();
+    //             if (str.size() > 2 && str != "100")
+    //                 moves_used.insert(str);
+    //         }
+    //     }
+    // }
+    // for(auto& specie :  data.at("Species").items())
+    // {
+    //     for (auto& form : specie.value().at("Forms").items())
+    //     {
+    //         for(json::iterator move = form.value()["EggMoves"].begin(); move != form.value()["EggMoves"].end(); move++)
+    //         {
+    //             //printf("%s\n", move->dump().c_str());
+    //             std::string str = move.value();
+    //             if (str.size() > 2 && str != "100")
+    //                 moves_used.insert(str);
+    //         }
+    //     }
+    // }
+
+    // if (all_moves != moves_used)
+    // {
+    //     printf("Moves not used: ");
+    //     for (auto move : all_moves)
+    //     {
+    //         if (!moves_used.count(move))
+    //         {
+    //             printf("%s, ", move.c_str());
+    //         }
+    //     }
+    //     printf("\nMove unknown: ");
+    //     for (auto move : moves_used)
+    //     {
+    //         if (!all_moves.count(move))
+    //         {
+    //             printf("%s, ", move.c_str());
+    //         }
+    //     }
+    // }
+    
+
 
     printf ("%s", data.dump(4).c_str());
 }
